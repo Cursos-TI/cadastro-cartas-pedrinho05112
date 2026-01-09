13a29acef9c70df265f4162b6dc5764bd979ac67
pedrinho05112 <155193656+pedrinho05112@users.noreply.github.com>
2026-01-09 17:48:47 +0000

    trabalho nivel Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,24 +12,31 @@ int main() {
   char codigo[20];
   char cidade[20];
   int população;
-  float Àrea;
+  float Area;
   float PIB;
   int turisticos;
+  float dens;
+  double carpita;
   //Área para armazenar dados na variaves
   printf("Digite o Estado de A-H:  ");
-  scanf("%c", &Estado);
+  scanf(" %c", &Estado);
   printf("Digite o codigo da carta:  ");
-  scanf("%s", codigo);
+  scanf(" %s", codigo);
   printf("Digite o nome da cidade:  ");
-  scanf("%s", cidade);
+  scanf("  %[^\n]", cidade);
   printf("Digite a quantidade da população:  ");
-  scanf("%d", &população);
+  scanf(" %d", &população);
   printf("Digite a Àrea:  ");
-  scanf("%f", &Àrea);
+  scanf("%f", &Area);
   printf("Digite o PIB:  ");
   scanf("%f", &PIB);
   printf("Digite o numero de pontos turísticos:  ");
   scanf("%d", &turisticos);
+  //Área que caulcula a densidade populacional e PIB per carpita
+  dens = (float) população / Area;
+  // Estou mutiplicando vezes 1 bilhão poruquê avriavel float não aguenta tantos numeros assim
+  carpita = (PIB * 1000000000.0 )  / população;
+
   //Espaço para separar
   printf("\n");
   //Área que printa os dados das cartas
@@ -38,9 +45,11 @@ int main() {
   printf("Codigo: %s\n", codigo);
   printf("Nome da Cidade: %s\n", cidade);
   printf("População: %d\n", população);
-  printf("Àrea: %.2f Km\n", Àrea);
-  printf("PIB: %.2f bilhões e reais\n", PIB);
+  printf("Àrea: %.2f Km\n", Area);
+  printf("PIB: %.2f bilhões de reais\n", PIB);
   printf("Número de pontos Turísticos: %d\n", turisticos);
+  printf("Densidade Populacional: %.2fhab/km\n", dens);
+  printf("PIB per carpita: %.2f reais\n", carpita);
   //Espaço para separar
   printf("\n");
   //Área para definir variaveis da segunda carta
@@ -49,24 +58,30 @@ int main() {
   char codigo_2[20];
   char cidade_2[20];
   int população_2;
-  float Àrea_2;
+  float Area_2;
   float PIB_2;
   int turisticos_2;
+  float dens_2;
+  double carpita_2;
   //Área para armazenar os dados das cartas
   printf("Digite o Estado de A-H:  ");
   scanf(" %c", &Estado_2);
   printf("Digite o codigo da carta:  ");
-  scanf("%s", codigo_2);
+  scanf(" %s", codigo_2);
   printf("Digite o nome da cidade:  ");
-  scanf("%s", cidade_2);
+  scanf(" %[^\n]", cidade_2);
   printf("Digite a quantidade da população:  ");
   scanf("%d", &população_2);
   printf("Digite a Àrea:  ");
-  scanf("%f", &Àrea_2);
+  scanf("%f", &Area_2);
   printf("Digite o PIB:  ");
   scanf("%f", &PIB_2);
   printf("Digite o numero de pontos turísticos:  ");
   scanf("%d", &turisticos_2);
+  //Área que caulcula a densidade populacional e PIB per carpita
+  dens_2 = (float) população_2 / Area_2;
+  carpita_2 = (PIB_2 * 1000000000.0) / população_2;
+
   //Espaço para separar
   printf("\n");
   //print das informações da segunda da carta
@@ -75,9 +90,11 @@ int main() {
   printf("Codigo: %s\n", codigo_2);
   printf("Nome da Cidade: %s\n", cidade_2);
   printf("População: %d\n", população_2);
-  printf("Àrea: %.2f Km\n", Àrea_2);
+  printf("Àrea: %.2f Km\n", Area_2);
   printf("PIB: %.2f bilhões e reais\n", PIB_2);
   printf("Número de pontos Turísticos: %d\n", turisticos_2);
+  printf("Densidade Populacional: %.2f hab/km\n", dens_2);
+  printf("PIB per carpita: %.2f reais\n", carpita_2);
 
   return 0;
 
